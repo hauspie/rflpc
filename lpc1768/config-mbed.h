544f1eed6a757827afbec221a81a285763c0cf80
Michael Hauspie <Michael.Hauspie@lifl.fr>
2011-07-11 17:32:01 +0200

    fixed missing file

---
+++ lpc1768/config-mbed.h
@@ -0,0 +1,30 @@
+/* This file is part of rflpc.                        
+ *									 
+ * rflpc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by	 
+ * the Free Software Foundation, either version 3 of the License, or	 
+ * (at your option) any later version.					 
+ * 									 
+ * rflpc is distributed in the hope that it will be useful,		 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of	 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	 
+ * GNU General Public License for more details.				 
+ * 									 
+ * You should have received a copy of the GNU General Public License	 
+ * along with rflpc.  If not, see <http://www.gnu.org/licenses/>.	 
+ */
+
+/*
+  Author: Michael Hauspie <michael.hauspie@univ-lille1.fr>
+  Created: 
+  Time-stamp: <2011-07-04 11:30:46 (hauspie)>
+
+  This files provides the configuration of the lib for the mbed platform
+*/
+
+#ifndef __RFLPC_CONFIG_MBED_H__
+#define __RFLPC_CONFIG_MBED_H__
+
+#define RFLPC_PLATFORM_CONFIGURED
+
+#endif
