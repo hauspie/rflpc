0d123149ec6fb02fe39b6d7875bfe67249e9659f
Michael Hauspie <Michael.Hauspie@lifl.fr>
2011-07-13 14:11:32 +0200

    fixed libname in GPL headers

--- sample/basic_tests/main.c
+++ sample/basic_tests/main.c
@@ -1,17 +1,17 @@
-/* This file is part of rfBareMbed.                        
+/* This file is part of rflpc.                        
  *									 
- * rfBareMbed is free software: you can redistribute it and/or modify
+ * rflpc is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by	 
  * the Free Software Foundation, either version 3 of the License, or	 
  * (at your option) any later version.					 
  * 									 
- * rfBareMbed is distributed in the hope that it will be useful,		 
+ * rflpc is distributed in the hope that it will be useful,		 
  * but WITHOUT ANY WARRANTY; without even the implied warranty of	 
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	 
  * GNU General Public License for more details.				 
  * 									 
  * You should have received a copy of the GNU General Public License	 
- * along with rfBareMbed.  If not, see <http://www.gnu.org/licenses/>.	 
+ * along with rflpc.  If not, see <http://www.gnu.org/licenses/>.	 
  */
 
 #include <debug.h>
@@ -305,7 +305,7 @@ int main()
 	RFLPC_STOP(RFLPC_LED_1 | RFLPC_LED_3, 1000000);
     test_uart();
 
-    printf("rfBareMbed sample test\r\n");
+    printf("rflpc sample test\r\n");
     printf("System clock is %d Hz\r\n", rflpc_clock_get_system_clock());
 
 /*    test_printf();*/
--- sample/ethernet/main.c
+++ sample/ethernet/main.c
@@ -1,17 +1,17 @@
-/* This file is part of rfBareMbed.                        
+/* This file is part of rflpc.                        
  *									 
- * rfBareMbed is free software: you can redistribute it and/or modify
+ * rflpc is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by	 
  * the Free Software Foundation, either version 3 of the License, or	 
  * (at your option) any later version.					 
  * 									 
- * rfBareMbed is distributed in the hope that it will be useful,		 
+ * rflpc is distributed in the hope that it will be useful,		 
  * but WITHOUT ANY WARRANTY; without even the implied warranty of	 
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	 
  * GNU General Public License for more details.				 
  * 									 
  * You should have received a copy of the GNU General Public License	 
- * along with rfBareMbed.  If not, see <http://www.gnu.org/licenses/>.	 
+ * along with rflpc.  If not, see <http://www.gnu.org/licenses/>.	 
  */
 
 #include <debug.h>
@@ -352,7 +352,7 @@ int main()
 {
     if (rflpc_uart0_init() == -1)
 	RFLPC_STOP(RFLPC_LED_1 | RFLPC_LED_3, 1000000);
-    printf("rfBareMbed ethernet sample\r\n");
+    printf("rflpc ethernet sample\r\n");
 
     ethernet();
 
