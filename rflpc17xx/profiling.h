b1560ea99c8a5bea63e18c0b5c43a1bc53ba90cd
Michael Hauspie <Michael.Hauspie@lifl.fr>
2011-09-29 14:52:00 +0200

    Added small profiling macros

---
+++ rflpc17xx/profiling.h
@@ -0,0 +1,43 @@
+/* This file is part of rflpc. Copyright 2010-2011 Michael Hauspie
+ *
+ * rflpc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rflpc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rflpc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * Author: Michael Hauspie <michael.hauspie@univ-lille1.fr>
+ * Created: 2011-09-29
+ * Time-stamp: <2011-09-29 14:18:07 (hauspie)>
+ */
+#ifndef __RFLPC_PROFILING_H__
+#define __RFLPC_PROFILING_H__
+
+#ifdef RFLPC_ENABLE_PROFILE
+
+#include "drivers/timer.h"
+
+#ifndef RFLPC_PROFILE_PRESCALE
+   #define RFLPC_PROFILE_PRESCALE (rflpc_clock_get_system_clock() / 1000000)
+#endif
+
+#define RFLPC_PROFILE_INIT(timer) do {rflpc_timer_enable(timer); rflpc_timer_set_clock(timer, RFLPC_CCLK); rflpc_timer_set_pre_scale_register(timer,RFLPC_PROFILE_PRESCALE); rflpc_timer_start(timer); } while(0)
+
+#define RFLPC_PROFILE_DECLARE_COUNTER(counter) uint32_t total_##counter;uint32_t start_##counter;
+
+#define RFLPC_PROFILE_START_COUNTER(counter,timer) do { start_##counter = rflpc_timer_get_counter(timer);} while(0)
+
+#define RFLPC_PROFILE_STOP_COUNTER(counter,timer) do { total_##counter += rflpc_timer_get_counter(timer) - start_##counter;} while(0)
+
+#define RFLPC_PROFILE_GET_TOTAL(counter) total_##counter
+
+#endif
+#endif
