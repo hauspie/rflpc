4b1546c824e06979517ece7ee5f5f99f1db7afc1
Michaël Hauspie <michael.hauspie@lifl.fr>
2013-12-17 17:35:45 +0100

    Updates the fault handler sample

--- samples/test_exception_handler/main.c
+++ samples/test_exception_handler/main.c
@@ -16,36 +16,43 @@
 /*
   Author: Michael Hauspie <michael.hauspie@univ-lille1.fr>
   Created:
-  Time-stamp: <2013-12-17 17:04:55 (hauspie)>
+  Time-stamp: <2013-12-17 17:34:45 (hauspie)>
 */
 #include <rflpc17xx/rflpc17xx.h>
 
+/* The naked attribute is needed so that the stack frame is clean
+   when entering the handler. This is the only way to get
+   the address of the svc instruction responsible for the exception 
+   in a consitent way
+*/
 RFLPC_IRQ_HANDLER svc_handler()  __attribute__( ( naked ) );
 RFLPC_IRQ_HANDLER svc_handler()
 {
     uint32_t *sp;
+    /* Get the stack pointer first to get the real stack frame */
     RFLPC_ARM_GET_REGISTER(sp,(uint32_t)sp);
+    /* As the function is naked, we have to save registers ourselves. Push all
+     * as we do not really know which registers will be used by the function */
+    __ASM volatile ("push {r0-r12,lr}"); 
+
     printf("SVC Handler\r\n");
+    /* sp[6] holds the return address of the svc call, SVC is a two bytes
+     * intruction so sp[6]-2 is the address of the svc instruction
+     */
     printf("SVC instruction address %p\r\n", sp[6]-2);
+    printf("PC instruction address %p\r\n", sp[6]);
     printf("SVC call param: 0x%x\r\n", *((uint16_t*)(sp[6]-2)) & 0xff);
     RFLPC_DUMP_STACK();
+    __ASM volatile ("pop {r0-r12,pc}"); /* this generates a return by poping the old lr value into PC */
 }
 
 
 void func(void)
 {
+    printf("Making a svc call\r\n");
     __ASM volatile ("svc 0x32\n");
-    __ASM volatile ("nop\n");
-    __ASM volatile ("nop\n");
-    __ASM volatile ("nop\n");
-    __ASM volatile ("nop\n");
-    __ASM volatile ("nop\n");
-    __ASM volatile ("nop\n");
-    __ASM volatile ("nop\n");
-    __ASM volatile ("nop\n");
-    __ASM volatile ("nop\n");
-    __ASM volatile ("nop\n");
-    __ASM volatile ("nop\n");
+    printf("Returned from svc\r\n");
+    printf("Will generate a fault now\r\n");
     *((int*)0) = 0;
     __ASM volatile ("nop\n");
     __ASM volatile ("nop\n");
@@ -54,7 +61,7 @@ void func(void)
     __ASM volatile ("nop\n");
     __ASM volatile ("nop\n");
     __ASM volatile ("nop\n");
-    __ASM volatile ("nop\n");
+    
 }
 
 int main()
