5ad8837141afc469e3c467307b1bf7b6c7608056
bacara <christophe.bacara@live.fr>
2014-04-08 05:30:10 +0200

    Added DAC driver files.

---
+++ rflpc17xx/drivers/dac.h
@@ -0,0 +1,48 @@
+/* This file is part of rflpc. Copyright 2010-2011 Michael Hauspie
+ *
+ * rflpc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rflpc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rflpc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ * Digital To Analog Converter (DAC) driver
+ */
+
+#ifndef __RFLPC_DAC_H__
+#define __RFLPC_DAC_H__
+
+/*
+  Author: Christophe Bacara <christophe.bacara@etudiant.univ-lille1.fr
+  Created: 2014-01-29
+
+  DAC driver for LPC.
+*/
+
+#ifdef RFLPC_CONFIG_ENABLE_DAC
+
+#include <rflpc17xx/rflpc17xx.h>
+
+/** @addtogroup dac DAC
+ * @ingroup drivers
+ * @{
+ */
+
+
+
+
+
+#endif /* RFLPC_CONFIG_ENABLE_DAC */
+
+/** @} */
+
+#endif /* __RFLPC_DAC_H__ */
